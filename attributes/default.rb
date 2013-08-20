dfe96cc90e889df5c7f90d873414b9d1db1cd359
Benjamin Vialle <benjamin@vialle.io>
2013-08-20 14:59:13 +0200

    Added recipe to build subversion from sources

---
+++ attributes/default.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: subversion
+# Attributes:: default
+#
+# Licensed under the GPL License, Version 3.0 (the "License");
+# you may not use this file except in compliance with the License.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+default[:subversion][:version]            = "1.8.1"
+default[:subversion][:download_link]      = "http://archive.apache.org/dist/subversion"
+default[:subversion][:ruby_sitedir]       = "/usr/local/lib/site_ruby"
--- recipes/source.rb
+++ recipes/source.rb
@@ -16,3 +16,26 @@ when "debian"
   end
 
 end
+
+remote_file "#{Chef::Config[:file_cache_path]}/subversion-#{node[:subversion][:version]}.tar.gz" do
+  source "#{node[:subversion][:download_link]}/subversion-#{node[:subversion][:version]}.tar.gz"
+  action :create_if_missing
+end
+
+bash "Compile subversion #{node[:subversion][:version]}" do
+  cwd Chef::Config[:file_cache_path]
+  code <<-EOH
+    tar xvzf subversion-#{node[:subversion][:version]}.tar.gz
+    cd subversion-#{node[:subversion][:version]}
+    ./configure --with-ruby-sitedir=#{node[:subversion][:ruby_sitedir]}
+    make && make swig-rb && make install && make install-swig-rb
+  EOH
+  creates "/usr/local/lib/site_ruby/1.9.1/svn/client.rb"
+  creates "/usr/local/lib/site_ruby/1.9.1/svn/repos.rb"
+end
+
+execute "Running ldconfig" do
+  command "echo '/usr/local/lib' >> /etc/ld.so.conf && ldconfig"
+  action :run
+  not_if "grep '/usr/local/lib' /etc/ld.so.conf"
+end
